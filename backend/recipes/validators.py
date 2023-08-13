432494468ca57bbfc9168de3f376a87041b1ad46
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-08-13 23:43:07 +0300

    Added validators

---
+++ backend/recipes/validators.py
@@ -0,0 +1,40 @@
+from django.core.validators import RegexValidator, MinValueValidator
+from django.utils.translation import gettext_lazy as _
+
+
+class Slug_Validator(RegexValidator):
+    regex = (r"^[-a-zA-Z0-9_]+$",)
+    message = (
+        _(
+            "Слаг может содержать только буквы, цифры, "
+            "знаки минус, подчеркивания и без пробелов."
+        ),
+    )
+
+
+class Hex_Validator(RegexValidator):
+    regex = (r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",)
+    message = ("Цвет должен быть в формате HEX-кода (например, #49B64E).",)
+
+
+class CookingTime_Validator:
+    def __init__(self, min_value=1, max_value=360):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __call__(self, value):
+        if value < self.min_value:
+            raise ValueError(
+                f"Время приготовления не может быть меньше "
+                f"{self.min_value} минуты."
+            )
+        if value > self.max_value:
+            raise ValueError(
+                f"Время приготовления не должно превышать "
+                f"{self.max_value} минут."
+            )
+
+
+class IngredientAmount_Validator(MinValueValidator):
+    limit_value = (1,)
+    message = f"Количество не может быть меньше {limit_value}"
