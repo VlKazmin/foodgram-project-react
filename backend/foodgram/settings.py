a9cddc20a850f1f5cccb67bf8eab249ad6295b63
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-08-23 00:04:23 +0300

    Update

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,14 +1,18 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    "django-insecure-mgv9b%zn#x-m#25dew0&3^lq$-*y#2a2t4^d+b8p$0h75mhz9a"
-)
+SECRET_KEY = os.getenv("SECRET_KEY", "default")
 
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG", False))
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ("127.0.0.1 localhost")).split()
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -17,13 +21,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_filters",
+    "djoser",
     "rest_framework",
     "rest_framework.authtoken",
-    "djoser",
-    "django_filters",
-    "api.apps.ApiConfig",
-    "recipes.apps.RecipesConfig",
-    "users.apps.UsersConfig",
+    "api",
+    "recipes",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -59,24 +63,16 @@ TEMPLATES = [
 WSGI_APPLICATION = "foodgram.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "django"),
+        "USER": os.getenv("POSTGRES_USER", "django"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", 5432),
     }
 }
-# "default": {
-#         "ENGINE": config("DB_ENGINE", default="django.db.backends.postgresql"),
-#         "NAME": config("DB_NAME", default="postgres"),
-#         "USER": config("POSTGRES_USER", default="postgres"),
-#         "PASSWORD": config("POSTGRES_PASSWORD", default="postgres"),
-#         "HOST": config("DB_HOST", default="127.0.0.1"),
-#         "PORT": config("DB_PORT", default=5432, cast=int),
-#     }
-# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -100,13 +96,13 @@ TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
 
-CSV_FILE_PATH = BASE_DIR.parent / "data/"
+INGREDIENT_CSV_FILE_PATH = BASE_DIR / "data/"
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "collected_static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_ROOT = BASE_DIR / "/media"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
