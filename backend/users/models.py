b9d7b6405466e05d7bb2e94f88266d64b7cf0636
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-08-29 23:53:28 +0300

    added username and required fields for model user

--- backend/users/models.py
+++ backend/users/models.py
@@ -35,6 +35,9 @@ class User(AbstractUser):
         max_length=150,
     )
 
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
